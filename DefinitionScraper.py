4b1e73a6a5f8c7cf1dd36c7cf1d93ed89da2840b
Robert <109625312+z00mm00z@users.noreply.github.com>
2022-09-23 20:58:28 +0200

    Version 1.0
    Refactored Code
    Handling for definitions that are not found
    Output to CSV finished

--- DefinitionScraper.py
+++ DefinitionScraper.py
@@ -3,40 +3,47 @@ import requests
 import csv
 
 exceptions = []
-exceptionNo = 0
+words = []
+definitions = []
+exceptionsCount = 0
 
 dataFile = open('WORD_DATA_LEN_5.csv', 'r')
 outputWordFile = open('OUTPUT_DATA.csv', 'w')
 type(dataFile)
 
 csvreader = csv.reader(dataFile, delimiter=',')
-csvwriterWords = csv.writer(outputWordFile, delimiter=',')
+csvwriter = csv.writer(outputWordFile, delimiter=',')
 
 print("If Google's HTML code is changed, this script may need to be updated.")
 print("Definitions are scraped from Google, Dictionary.com and Merriam Webster.")
+
+def Scrape(query, defIn, source):
+    print(query)
+    words.append(query)
+    definitions.append(defIn)
+    print('Source: ' + source)
+    print(defIn)
+
 
 for row in csvreader:
     query = row[0]
     
     print('---------')
-    print(query)
     try:
         url = f'https://google.com/search?q={query}+definition&oq=help+defin&aqs=chrome.0.69i59j69i57l3j0i271j69i60l3.1869j0j7&sourceid=chrome&ie=UTF-8'
         r = requests.get(url)
         soup = BeautifulSoup(r.content, "html.parser")
-
         definition = soup.find('div', attrs={'class':'BNeawe s3v9rd AP7Wnd'}).find('div', attrs={'class':'v9i61e'}).get_text()
-        print('Source: https://google.com')
-        print(definition)
-    except AttributeError as e:
+
+        Scrape(query, definition, 'https://google.com')
+    except (AttributeError, requests.exceptions.TooManyRedirects) as e:
         try:
             url = f'https://dictionary.com/browse/{query}'
             r = requests.get(url)
             soup = BeautifulSoup(r.content, "html.parser")
 
             definition = soup.find('span', attrs={'class':'one-click-content css-nnyc96 e1q3nk1v1'}).get_text()
-            print('Source: https://dictionary.com')
-            print(definition)
+            Scrape(query, definition, 'https://dictionary.com')
         except (AttributeError, requests.exceptions.TooManyRedirects) as e:
             try:
                 url = f'https://merriam-webster.com/dictionary/{query}'
@@ -45,20 +52,26 @@ for row in csvreader:
 
                 definition = soup.find('span', attrs={'class':'dtText'}).get_text()
                 definition = definition.replace(": ", "")
-                print('Source: https://merriam-webster.com')
-                print(definition)
-            except AttributeError:
+
+                Scrape(query, definition, 'https://merriam-webster.com')
+            except (AttributeError, requests.exceptions.TooManyRedirects):
                 print('FAILED: definition for "' + str(query) + '" could not be found.')
-                exceptionNo += 1
+                exceptionsCount += 1
                 exceptions.append(query)
-    except requests.exceptions.TooManyRedirects: 
-        print('Too many redirects') 
         
-
-
-
-for i in exceptions:
-    print(exceptions[i])
+for i in range(len(words)):
+    csvwriter.writerow([words[i], definitions[i]])
+    
+if exceptionsCount != 0:
+    print('---------')
+    print(str(exceptionsCount) + ' definition(s) could not be found.')
+    answer = input('Add now? (y/n): ', )
+    if answer == 'y':
+        for exception in exceptions:
+            print(exception)
+            input = input('Definition: ',)
+            csvwriter.writerow([exception, input])
+            
 
 dataFile.close()
 outputWordFile.close()
